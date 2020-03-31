6c4d58be2be48c5ae11764378dd1afae7794ba50
encodedBicoding <dominicisioma000@gmail.com>
2020-03-31 18:33:49 +0100

    add url

--- app/config/config.js
+++ app/config/config.js
@@ -16,7 +16,7 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    use_env_variable: process.env.DATABASE_URL,
+    use_env_variable: 'postgres://rxlbpkdywezojw:d747179ed45596689d155b74f1d55ec891438991038eec7a1b80710aecd9cc86@ec2-54-159-112-44.compute-1.amazonaws.com:5432/ddicb31jqo3jmr',
     logging: false,
     dialect: 'postgres'
   }

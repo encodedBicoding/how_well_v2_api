d0b470882fbdce60e8e757a0898015e108cf30c7
encodedBicoding <dominicisioma000@gmail.com>
2020-03-31 19:01:26 +0100

    update envd

--- app/models/index.js
+++ app/models/index.js
@@ -12,9 +12,8 @@ const db = {};
 dotenv.config();
 
 let sequelize;
-
 if (config.use_env_variable) {
-  sequelize = new Sequelize(config.use_env_variable, config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }

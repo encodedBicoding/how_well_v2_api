d07f6379217b8467d21fa5fb2b087f253e1b09a7
encodedBicoding <dominicisioma000@gmail.com>
2020-04-01 18:27:01 +0100

    removes cors to connect api

--- app/server.js
+++ app/server.js
@@ -1,6 +1,5 @@
 require('babel-polyfill');
 const express = require('express');
-const cors = require('cors');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const { config } = require('dotenv');
@@ -23,7 +22,6 @@ const isProduction = app.get('env') === 'production';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false, limit: '50mb'}))
-app.use(cors());
 if (isProduction) app.use(enforce.HTTPS());
 if (!isProduction) {
   app.use(errorHandler({

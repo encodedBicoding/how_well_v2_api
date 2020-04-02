b881e2001c4607d41fa23cc0c7b89c1e598e80c1
encodedBicoding <dominicisioma000@gmail.com>
2020-04-02 13:02:24 +0100

    add answers to user questions

--- app/models/responses.js
+++ app/models/responses.js
@@ -22,7 +22,7 @@ module.exports = (sequelize, DataTypes) => {
   Responses.associate = function(models) {
     // associations can be defined here
     Responses.belongsTo(models.Questions, {
-      foreignKey: 'questionId'
+      foreignKey: 'questionId',
     })
   };
   return Responses;
